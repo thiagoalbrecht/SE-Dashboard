7308281492eeb9ad9334ca7fef451ddd48cade32
Thiago Albrecht <thisalviano@hotmail.com>
2022-01-27 20:14:00 +0100

    Added functions to settings menu

--- app.js
+++ app.js
@@ -1,34 +1,47 @@
 var rootURL = "https://smartenv.salviano.xyz";
+var controller = new AbortController();
+var doingRequest = false;
 var dashboardApp = new Vue({
 
     el: '#SEDashboard',
 
     data: {
-        level1: 'XX',
-        level2: 'XX',
-        level3: 'XX'
+        level1: '0',
+        level2: '0',
+        level3: '0',
+        lastdate: '00-00-0000 00:00:00',
+        liveUpdate: true
     },
 
     methods: {
 
-        refreshItems: function (instant) {
+        refreshItems: function (instant, repeat) {
             task = "/get.php";
             if (instant) task = "/get.php?instant=1";
-            axios.get(rootURL + task)
-                .then(response => {
-                    //this.level = JSON.parse(response.data);
-                    this.level1 = response.data['sensor_value_1'];
-                    this.level2 = response.data['sensor_value_2'];
-                    this.level3 = response.data['sensor_value_3'];
-                    
-                    console.log(this.level);
-                    this.refreshItems();
-                })
+            if (!doingRequest) {
+                doingRequest = true;
+                axios.get(rootURL + task)
+                    .then(response => {
+                        doingRequest = false;
+                        this.level1 = response.data['sensor_value_1'];
+                        this.level2 = response.data['sensor_value_2'];
+                        this.level3 = response.data['sensor_value_3'];
+                        this.lastdate = response.data['datetime'];
+                        if (this.liveUpdate && repeat) {
+                            this.refreshItems(false, true);
+                            console.log("Print");
+                        }
+                    }).catch(err => {
+                        doingRequest = false;
+                        console.log(err);
+                    });
+            }
+
         },
 
     },
 
     mounted() {
-        this.refreshItems(true);
+        this.refreshItems(true, true);
     }
 });
