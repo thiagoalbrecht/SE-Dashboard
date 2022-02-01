752c524b27ca40ca2a92b360bf408d8d5fe8b5b0
Thiago Albrecht <thisalviano@hotmail.com>
2022-02-01 21:38:00 +0100

    Commited credentials.php

---
+++ secret/credentials.php
@@ -0,0 +1,9 @@
+<?php
+// Set the credentials for MySQL database
+define("servername", "localhost");
+define("username", "#####");
+define("password", "#####");
+define("dbname", "#####");
+
+// Password for processing
+define ("ProcessingPassword", "#####");
